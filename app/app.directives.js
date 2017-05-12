505e43ce325e7ea2e03896933d973e18944ebf11
Adam Kocsis <kocccs@gmail.com>
2017-05-12 19:54:39 +0200

    updateBackground directive is fixed

--- app/app.directives.js
+++ app/app.directives.js
@@ -23,13 +23,30 @@ app.directive("enterExecute", function() {
     };
 });
 
-app.directive('updateBackground', function() {
+app.directive('updateBackground', function($http) {
+    function _arrayBufferToBase64(buffer) {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+
     return {
         restrict: 'AE',
         replace: true,
         link: function(scope, elem, attrs) {
             setInterval(function() {
-                elem.css("background-image", 'url("https://source.unsplash.com/3840x2160?nature,landscape")');
+                $http({
+                    method: 'GET',
+                    url: 'https://source.unsplash.com/3840x2160?nature,landscape',
+                    responseType: 'arraybuffer'
+                }).then(function(response){
+                    var str = _arrayBufferToBase64(response.data);
+                    elem.css("background-image", 'url("data:image/jpg;base64,' + str + '")');
+                });
             }, 10000);
         }
     };
