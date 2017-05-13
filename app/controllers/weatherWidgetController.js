81c10973303740678cf79d2be8e95a309673f50e
Adam Kocsis <kocccs@gmail.com>
2017-05-13 17:52:46 +0200

    Weahter widget is implemented

--- app/controllers/weatherWidgetController.js
+++ app/controllers/weatherWidgetController.js
@@ -1,10 +1,26 @@
 (function(){
     'use strict';
     
-    angular.module("app")
-    .controller("weatherWidgetController", ["$timeout", "$scope",
-        function($timeout, $scope) {
+    angular.module('app')
+    .controller('weatherWidgetController', ['$timeout', '$scope', '$http',
+        function($timeout, $scope, $http) {
             $scope.title='Weather widget placeholder';
+            var wuUrl = 'http://api.wunderground.com/api/#apiKey#/conditions/q/#location#.json';
+            var wuApiKey = 'f116d809a1ca39ac'; // TODO: register new key
+            var location = '47.493678,19.074948';
+            var updateIntervalMinutes = 5;
+
+            $scope.currentObservation = {};
+            updateWeather();
+
+            $timeout(updateWeather, updateIntervalMinutes * 60000);
+
+            function updateWeather() {
+                var url = wuUrl.replace('#apiKey#', wuApiKey).replace('#location#', location);
+                $http.get(url).then(function(response){
+                    $scope.currentObservation = response.data.current_observation;
+                });
+            }
         }
     ]);
 })();
