5599dd5cb9ab5a37187949a6155a81d76063490a
Balázs Kis <uibali@gmail.com>
2017-05-16 14:04:07 +0200

    #2 Adding files for clock widget

---
+++ app/controllers/clockWidgetController.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular.module('app')
+        .controller('clockWidgetController', ['$timeout', '$scope',
+            function($timeout, $scope) {
+
+            }
+        ]);
+})();
