a716019e42d908964139b971d15662c6c51bacfc
Adam Kocsis <kocccs@gmail.com>
2017-05-17 21:18:00 +0200

    forecast
    weather widget now supports forecast data.
    on click and on every update the details section is changed to forecast and back.

--- app/controllers/weatherWidgetController.js
+++ app/controllers/weatherWidgetController.js
@@ -5,20 +5,31 @@
         .controller('weatherWidgetController', ['$interval', '$scope', '$http',
             function($interval, $scope, $http) {
                 $scope.title = 'Weather widget';
-                var wuUrl = 'http://api.wunderground.com/api/#apiKey#/conditions/q/#location#.json';
+                var wuUrl = 'http://api.wunderground.com/api/#apiKey#/conditions/forecast/q/#location#.json';
                 var wuApiKey = '793e759bef2186c4';
                 var location = '47.493678,19.074948';
                 var updateIntervalMinutes = 5;
 
                 $scope.currentObservation = {};
+                $scope.forecast = {};
                 updateWeather();
 
                 $interval(updateWeather, updateIntervalMinutes * 60000);
+
+                $scope.switchDetails = function(){
+                    if ($scope.detailsType === 'weatherDetails') {
+                        $scope.detailsType = 'forecast';
+                    } else {
+                        $scope.detailsType = 'weatherDetails';
+                    }
+                };
 
                 function updateWeather() {
                     var url = wuUrl.replace('#apiKey#', wuApiKey).replace('#location#', location);
                     $http.get(url).then(function(response) {
                         $scope.currentObservation = response.data.current_observation;
+                        $scope.forecast = response.data.forecast;
+                        $scope.switchDetails();
                     });
                 }
             }
