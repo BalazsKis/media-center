bebb0a4dad29c05ed6cbf7edd4160c67cbdafb31
Balázs Kis <uibali@gmail.com>
2017-06-01 11:15:33 +0200

    #4 CPU and RAM usage logged to console

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,12 +1,26 @@
-(function(){
+(function() {
     'use strict';
-    
-    angular.module("app")
-    .controller("homeController", ["$scope", "$timeout", "moment",
-        function($scope, $timeout, moment) {
-            
-            //TODO: Write home functionality here.
 
-        }
-    ]);
+    angular.module("app")
+        .controller("homeController", ["$scope", "$timeout", "moment",
+            function($scope, $timeout, moment) {
+
+                //TODO: Write home functionality here.
+
+                var osUtils = require('os-utils');
+
+                function logSysInfo() {
+
+                    console.log('RAM Usage (%): ' + ((1 - osUtils.freememPercentage()) * 100));
+
+                    osUtils.cpuUsage(function(v) {
+                        console.log('CPU Usage (%): ' + v * 100);
+                    });
+                    setTimeout(logSysInfo, 2000);
+                }
+
+                logSysInfo();
+
+            }
+        ]);
 })();
